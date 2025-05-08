e137a237d59eb30a2a6354d55571c0975f900bba
Your Name <you@example.com>
2025-05-08 15:31:14 +0500

    feat: add experts responsive section

--- main.js
+++ main.js
@@ -136,6 +136,38 @@ $(document).ready(function () {
                       },
             ]
         }
+      }else if (slider.hasClass('experts')) {
+        extraOptions = {
+          slidesToShow: 3.5,
+            slidesToScroll: 1,
+            centerMode: false,
+            arrows: true,
+            dots: false,
+            autoPlay: true,
+            responsive: [
+              {
+                breakpoint: 1400,
+                settings: {
+                  slidesToShow: 2.5,
+                  slidesToScroll: 2,
+                }
+              },
+              {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 1.5,
+                  slidesToScroll: 2,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+                      },
+            ]
+        }
       }
     slider.slick($.extend({}, extraOptions, options ));
   })
