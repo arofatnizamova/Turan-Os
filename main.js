b3fe63064ee0ded68490e04783101cd38a3913bb
Your Name <you@example.com>
2025-05-13 14:08:52 +0500

    feat: add local registration page

--- main.js
+++ main.js
@@ -200,6 +200,38 @@ $(document).ready(function () {
                       },
             ]
         }
+      }else if (slider.hasClass('get')) {
+        extraOptions = {
+          slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: false,
+            arrows: true,
+            dots: false,
+            autoPlay: true,
+            responsive: [
+              {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 2.8,
+                  slidesToScroll: 2,
+                }
+              },
+              {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 1.5,
+                  slidesToScroll: 2,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+                      },
+            ]
+        }
       }
     slider.slick($.extend({}, extraOptions, options ));
   })
